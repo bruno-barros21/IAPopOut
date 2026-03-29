331fa5a151ff8f60405b0d6d6e56600720cd7456
bruno-barros21 <fcportobb@gmail.com>
2026-03-29 17:13:42 +0100

    dataset partial

--- src/ml/__init__.py
+++ src/ml/__init__.py
@@ -1,6 +1,2 @@
 from .decision_tree import DecisionTreeID3
-from .dataset import (
-    generate_dataset, board_to_features, move_to_label, label_to_move,
-    save_dataset, load_dataset, DATA_DIR, POPOUT_DATASET_PATH, IRIS_DATASET_PATH,
-    FEATURE_NAMES, N_LABELS,
-)
+from .dataset import generate_dataset, board_to_features, move_to_label, label_to_move
